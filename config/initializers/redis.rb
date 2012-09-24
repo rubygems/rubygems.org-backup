c5b462cbe9bf83c37e89e1f2ff7915dc8009a459
Evan Phoenix <evan@fallingsnow.net>
2012-09-23 21:42:27 -0700

    Connect to redis on db1

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,6 +3,8 @@ if Rails.env.test? || Rails.env.cucumber?
 elsif Rails.env.recovery?
   require "fakeredis"
   $redis = Redis.new
+elsif Rails.env.production
+  $redis = Redis.connect(:host => "db1")
 else
   $redis = Redis.connect(:url => ENV['REDISTOGO_URL'])
 end
